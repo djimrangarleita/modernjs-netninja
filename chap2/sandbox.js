30035128a2f49c1e184709ffa6f25a7371377e3a
djimrangarleita <dngarleita@gmail.com>
2023-07-20 14:24:07 +0300

    JS hoisting

--- chap2/sandbox.js
+++ chap2/sandbox.js
@@ -1,5 +1,16 @@
 let email = 'dngarleita@gmail.com'
+let uname = 'Djimra NGARLEITA'
 
-let sstringMeth = email.substring(4,10);
+let result = `The email address of ${uname} is ${email}`;
 
-console.log(sstringMeth);
+console.log(result.includes('z'));
+
+greet('Djimra');
+// Function declaration: Hoisting apply
+function greet(name){
+    console.log(`Hello ${name}`);
+}
+
+// Function expression no Hoisting
+const speak = (name) => console.log(`${name} is speaking`)
+speak('Djimra');
