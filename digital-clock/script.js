c9d33b68d16d58d63c9ab5043e793283e8f0dba4
djimrangarleita <dngarleita@gmail.com>
2023-07-23 01:27:51 +0300

    Optimized version of digital clock

--- digital-clock/script.js
+++ digital-clock/script.js
@@ -3,5 +3,5 @@ const digitalClock = document.querySelector('.digital-clock');
 
 setInterval(() => {
     const date = new Date();
-    digitalClock.textContent = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    digitalClock.textContent = date.toLocaleTimeString();
 }, 1000);
